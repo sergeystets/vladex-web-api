8c4f3c883325693ea258cc47a2d2c7ebaeb12610
sergeystets <sergeystets@gmail.com>
2021-01-04 23:29:36 +0300

    reverted ability to pack app as war

--- src/main/java/com/sergeystets/vladex/web/api/VladexWebApiApplication.java
+++ src/main/java/com/sergeystets/vladex/web/api/VladexWebApiApplication.java
@@ -2,18 +2,11 @@ package com.sergeystets.vladex.web.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VladexWebApiApplication extends SpringBootServletInitializer {
+public class VladexWebApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VladexWebApiApplication.class, args);
-    }
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(VladexWebApiApplication.class);
     }
 }
