c44540ea9aeda05cb12f9e7d903ef56adad9abbe
sergeystets <sergeystets@gmail.com>
2021-01-16 19:47:08 +0200

    added user_id claim to a token

--- src/main/java/com/sergeystets/vladex/web/api/config/ws/SessionConnectedEventListener.java
+++ src/main/java/com/sergeystets/vladex/web/api/config/ws/SessionConnectedEventListener.java
@@ -1,10 +1,13 @@
 package com.sergeystets.vladex.web.api.config.ws;
 
 import com.sergeystets.vladex.web.api.model.ws.ClientPresence;
+import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 
@@ -13,12 +16,14 @@ import org.springframework.web.socket.messaging.SessionConnectedEvent;
 public class SessionConnectedEventListener implements ApplicationListener<SessionConnectedEvent> {
 
   @Autowired
-  private SimpMessagingTemplate simpMessagingTemplate;
+  private SimpMessagingTemplate messagingTemplate;
 
   @Override
   public void onApplicationEvent(SessionConnectedEvent event) {
     log.info("ws connected event {}", event);
-    simpMessagingTemplate.convertAndSend("/topic/presence",
-        new ClientPresence().setOnline(true).setUserId(2));
+    final Jwt token = ((JwtAuthenticationToken) Objects.requireNonNull(event.getUser())).getToken();
+    final long userId = (Long) token.getClaims().get("user_id");
+    final ClientPresence presence = new ClientPresence().setOnline(true).setUserId(userId);
+    messagingTemplate.convertAndSend("/topic/presence", presence);
   }
 }
