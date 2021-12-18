2557db051c1851a9fc08281264b94191e1bfba40
sergeystets <sergeystets@gmail.com>
2021-12-18 23:16:25 +0200

    optimized imports, added another entry to cors config

--- src/main/java/com/sergeystets/vladex/web/api/config/CorsConfig.java
+++ src/main/java/com/sergeystets/vladex/web/api/config/CorsConfig.java
@@ -1,17 +1,14 @@
 package com.sergeystets.vladex.web.api.config;
 
-import com.google.common.collect.ImmutableList;
-import java.util.Collections;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class CorsConfig {
--- src/main/java/com/sergeystets/vladex/web/api/config/WebSecurityConfig.java
+++ src/main/java/com/sergeystets/vladex/web/api/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.sergeystets.vladex.web.api.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
